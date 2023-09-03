2827f91af8564edd529bdaf44a2d0005dd75a530
Keith Gross <justKeith@users.noreply.github.com>
2023-09-03 10:48:15 -0500

    dfgh

--- Account.js
+++ Account.js
@@ -1,12 +1,33 @@
-export class Account {
+class Account {
     #name;
     #balanceDate;
     #balance;
     #history;
 
     constructor(name, balance) {
-        this.#name = name;
-        this.#balance = balance;
+        if (name instanceof Account) {
+            var original = name;
+
+            this.#name = original.getName();
+            this.#balance = original.getBalance();
+        } else {
+            this.#name = name;
+            this.#balance = balance;
+        }
+    }
+
+    // A history entry is at minimum required to have a balanceDate and a balance. All
+    // history is frozen so it can't be altered and the getHistory method can simply 
+    // return entries safely.
+    addHistory(entry) {
+        if (entry.balanceDate && (entry.balance !== undefined)) {
+            entry = Object.freeze(entry);
+            this.#history.push(entry);
+            this.#balanceDate = entry.balanceDate;
+            this.#balance = entry.balance;
+        } else {
+            throw "invalid call to addHistory. Either balanceDate or balance is missing " + JSON.stringify(entry) +")"
+        }
     }
 
     getName() {
@@ -20,6 +41,12 @@ export class Account {
     getBalance() {
         return (this.#balance);
     }
+
+    getHistory(index) {
+        var entry = this.#history[index];
+
+        return (entry);
+    }
 }
 
 // A account that pays a fixed interst the first day of each month.
@@ -27,15 +54,25 @@ export class FixedInterestAccount extends Account {
     #rate = 0.001;
 
     constructor(name, balance, rate) {
-        super(name, balance);
+        if (name instanceof Account) {
+            var original = name;
 
-        this.#rate = rate;
+            super(original);
+            this.#rate = original.getRate();
+        } else {
+            super(name, balance);
+
+            this.#rate = rate;
+        }
     }
 
-    start(date) {
-        var result = Object.create(this);
-        this.#balanceDate = date;
-        this.#history = [];
+    start(startDate) {
+        var result = new FixedInterestAccount(this);
+
+        result.addHistory({
+            balanceDate: startDate,
+            balance: this.getBalance()
+        })
     }
 
     step(date) {
